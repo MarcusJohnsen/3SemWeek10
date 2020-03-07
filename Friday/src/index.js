ecc9bd6cfadbce95f164b2e89d9060be16eb6550
Marcus Johnsen <cph-mj734@cphbusiness.dk>
2020-03-07 12:59:53 +0100

    first assignment friday

--- Friday/src/index.js
+++ Friday/src/index.js
@@ -1,8 +1,45 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
+document.getElementById("svg2").addEventListener("click", clickMap)
+let url = "http://restcountries.eu/rest/v1/alpha?codes="
 
+var prev
 
+function clickMap(element) {
+    if (prev == null) {
+        prev = element
+        element.target.style = "fill: red"
+    } else {
+        prev.target.style = "fill:606060"
+        element.target.style = "fill: red"
+        prev = element
+    }
 
+    let fetchURL
+    if (element.target.id != "svg2") {
+        if (element.target.id.length > 2) {
+            fetchURL = url + element.target.parentNode.id
+        } else {
+            fetchURL = url + element.target.id
+        }
+
+        fetch(fetchURL)
+            .then(function(response) {
+                return response.json()
+            }).then(function(info) {
+                console.log(info)
+                showDataAboutCountry(info)
+            });
+    } else {
+        document.getElementById("europeMap").innerHTML = "Not a country mate. Retry. And do better!"
+    }
+}
+
+function showDataAboutCountry(info) {
+    let countryStats =
+        "Country: " + info[0].name + "<br>" +
+        "Population: " + info[0].population + "<br>" +
+        "Area: " + info[0].area + "<br>" +
+        "Borders: " + info[0].borders.join(", ")
+    document.getElementById("europeMap").innerHTML = countryStats
+}
